0567818edc8108ab8dea591a09d74102caf86be1
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-11-14 16:35:08 -0800

    Initial commit.

---
+++ BRC-Ultrawide/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Reptile;
+
+namespace BRC_Ultrawide
+{
+    [BepInPlugin("com.MandM.BRC-Ultrawide", "BRC-Ultrawide", "1.0.0")]
+    [BepInProcess("Bomb Rush Cyberfunk.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("MandM.BRC-Ultrawide.Harmony");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(EffectsUI))]
+    public class EffectsUIPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("ShowVerticalBars")]
+        public static void ShowVerticalBars_Postfix(EffectsUI __instance)
+        {
+            // Immediately hide the vertical bars.
+            __instance.HideVerticalBars();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# BRC-Ultrawide
+A BepInEx plugin for [Bomb Rush Cyberfunk](https://store.steampowered.com/app/1353230) that removes the pillarboxing that appears during most cutscenes. Meant for ultrawide users, as given by the name.
+
+![Preview](Thunderstore/preview.png)
